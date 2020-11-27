be67046331b0c3add1ade091a1b1f0da35ff5ef3
Marco Lanfranchi <marco.lanfranchi@gninformatica.com>
2020-11-27 10:12:31 +0100

    Add publisher entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRpository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRpository publisherRpository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRpository publisherRpository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRpository = publisherRpository;
     }
 
     @Override
@@ -34,8 +38,12 @@ public class BootStrapData implements CommandLineRunner {
         authorRepository.save(auth2);
         bookRepository.save(book2);
 
+        Publisher pub1 = new Publisher("Editore1", "Via Milano, 4", "Lecco", "Italia", "23900");
+        publisherRpository.save(pub1);
+
         System.out.println("Started in BootStrap");
         System.out.println("Numero di libri: "+bookRepository.count());
+        System.out.println("Numero di publisher: "+publisherRpository.count());
 
     }
 }
